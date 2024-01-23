b17ea28a50cadd239d087a76e0f3c665e09c243d
Cecimartinez <martinezcecilia2908@gmail.com>
2024-01-23 12:01:11 -0300

    Added Interstitial Ads to Pokemon List Screen

--- components/Pokemons/PokemonsList.js
+++ components/Pokemons/PokemonsList.js
@@ -3,100 +3,114 @@ import { FlatList, View, Text, Image, StyleSheet, TouchableOpacity } from 'react
 import { connect } from 'react-redux';
 import { GET_POKEMONS_REQUEST } from '../../redux/actions';
 import PokemonDetailModal from './PokemonDetailModal';
+import { InterstitialAd, AdEventType, TestIds } from 'react-native-google-mobile-ads';
+
+const adUnitId = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-4209556911281829/7296853700';
+
+const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
+ keywords: ['pokemon', 'game'],
+});
 
 const PokemonsList = (props) => {
-
+ const [loaded, setLoaded] = useState(false);
  const [modalVisible, setModalVisible] = useState(false);
  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
-
-
-  useEffect(() => {
+ useEffect(() => {
     props.getPokemonsRequest();
-  }, []);
 
-  // console.log('Pokemons from Redux:', props.pokemons.result);
+    const unsubscribe = interstitial.addAdEventListener(AdEventType.LOADED, () => {
+      setLoaded(true);
+    });
 
-  const { pokemons, loading, error } = props;
+    interstitial.load();
 
-  if (loading) {
+    return unsubscribe;
+ }, []);
+
+ const { pokemons, loading, error } = props;
+
+ if (loading) {
     return <Text>Loading...</Text>;
-  }
+ }
 
-  if (error) {
+ if (error) {
     return <Text>Error: {error.message}</Text>;
-  }
-const renderItem = ({ item, index }) => {
-  // Construct the image URL using the Pokémon index (+1 as the API starts at index 1).
-  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`;
+ }
 
-  return (
-    <TouchableOpacity onPress={() => {
-      setSelectedPokemon(item);
-      setModalVisible(true);
-    }}>
-      <View style={styles.itemContainer}>
-        <Image
-          style={styles.pokemonImage}
-          source={{ uri: imageUrl }}
-        />
-        <Text style={styles.pokemonName}>{item.name}</Text>
-      </View>
-    </TouchableOpacity>
-  );
-};
+ const renderItem = ({ item, index }) => {
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`;
 
-  console.log(pokemons, "pokemons")
+    return (
+      <TouchableOpacity onPress={async () => {
+        setSelectedPokemon(item);
+        setModalVisible(true);
+        if (loaded) {
+          await interstitial.show();
+          interstitial.load();
+        } else {
+    await interstitial.show();
+ }
+      }}>
+        <View style={styles.itemContainer}>
+          <Image
+            style={styles.pokemonImage}
+            source={{ uri: imageUrl }}
+          />
+          <Text style={styles.pokemonName}>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+ };
 
-  return (
+ return (
     <>
-     <FlatList
-       data={pokemons}
-       renderItem={renderItem}
-       keyExtractor={(item) => item.name}
-     />
-     <PokemonDetailModal
-       visible={modalVisible}
-       pokemon={selectedPokemon}
-       onClose={() => setModalVisible(false)}
-     />
-   </>
-  );
+      <FlatList
+        data={pokemons}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.name}
+      />
+      <PokemonDetailModal
+        visible={modalVisible}
+        pokemon={selectedPokemon}
+        onClose={() => setModalVisible(false)}
+      />
+    </>
+ );
 };
 
 const mapStateToProps = (state) => {
-  return {
+ return {
     pokemons: state.pokemonsReducer.pokemons || [],
     loading: state.pokemonsReducer.loading,
     error: state.pokemonsReducer.error,
-  };
+ };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
+ return {
     getPokemonsRequest: () => 
     dispatch({ type: GET_POKEMONS_REQUEST }),
-  };
+ };
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(PokemonsList);
 
 const styles = StyleSheet.create({
-  itemContainer: {
+ itemContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
-  },
-  pokemonName: {
+ },
+ pokemonName: {
     fontSize: 20, 
     textTransform: 'capitalize', 
-  },
-  pokemonImage: {
+ },
+ pokemonImage: {
     width: 75, 
     height: 75, 
     marginRight: 15,
-  },
+ },
 });
